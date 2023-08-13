31fd8b3b9e7c9d815170a494933fbf5959599f31
Ana <aninha10.desouza@gmail.com>
2023-08-13 20:10:17 -0300

    Inserção Model Especie

--- amigosjvll/admin.py
+++ amigosjvll/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import Cor, Raca, Apelido
+from .models import Cor, Raca, Apelido, Especie
 
 # admin.site.register(Animal)
 admin.site.register(Cor)
-# admin.site.register(Especie)
+admin.site.register(Especie)
 # admin.site.register(Idade)
 admin.site.register(Apelido)
 admin.site.register(Raca)
--- amigosjvll/models/especie.py
+++ amigosjvll/models/especie.py
@@ -1,8 +1,8 @@
-# from django.db import models
+from django.db import models
 
 
-# class Especie(models.Model):
-#     descricao = models.CharField(max_length=100)
+class Especie(models.Model):
+    descricao = models.CharField(max_length=100)
 
-#     def __str__(self):
-#         return f"{self.descricao} ({self.id})"
+    def __str__(self):
+        return f"{self.descricao} ({self.id})"
