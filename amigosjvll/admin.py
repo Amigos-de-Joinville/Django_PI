9df36fc0a421a0d491f84deba3f206123cd56d43
Ana <aninha10.desouza@gmail.com>
2023-08-13 20:03:01 -0300

    Inserção Model Raca

--- amigosjvll/admin.py
+++ amigosjvll/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import Cor
+from .models import Cor, Raca
 
 # admin.site.register(Animal)
 admin.site.register(Cor)
 # admin.site.register(Especie)
 # admin.site.register(Idade)
 # admin.site.register(Nome)
-# admin.site.register(Raca)
+admin.site.register(Raca)
--- amigosjvll/models/__init__.py
+++ amigosjvll/models/__init__.py
@@ -2,5 +2,5 @@
 # from .especie import Especie
 # from .idade import Idade
 from .cor import Cor
-# from .raca import Raca
+from .raca import Raca
 # from .apelido import Apelidos
